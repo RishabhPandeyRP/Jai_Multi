b3a8ff25fbf2a38cf4f6e631982be9b9f9dada7e
RishabhPandeyRP <rishabh17704@gmail.com>
2024-10-06 13:34:03 +0530

    responsive 4

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -15,7 +15,7 @@ const ContactPage = ()=>{
                 <div className="relative w-full h-[60vh]  border-green-600 mb-[0%]">
                     <img src={heroImg} alt="heroImg" className="w-full h-full border brightness-[60%] object-cover -z-10 " />
 
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-newspaper text-[31px] md:text-[38px] text-white font-bold w-[20%] z-0 ">
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  font-newspaper text-[27px] md:text-[38px] text-white font-bold w-[54%] md:w-[30%] z-0 ">
                     Get In Touch:
                     Let's Start a Conversation
                     </div>
