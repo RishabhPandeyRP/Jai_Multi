8f4dd5cc881bc6bef72c620b1ebf3c63bbc0e2aa
RishabhPandeyRP <rishabh17704@gmail.com>
2024-10-14 17:00:26 +0530

    final changes 3

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -78,10 +78,10 @@ const AboutUs = () => {
                             </div>
                             <div className="w-fit flex gap-3 md:gap-5 flex-col font-raleway text-justify">
                                 <p>
-                                Rine Group of Industries is a premier manufacturing company specializing in the production of highly critical casting and electromechanical products for the Indian Railways. With an unwavering commitment to precision and quality, we deliver a range of essential components, including couplers, knuckles, axle boxes, alternators, oil cooling units, and control panels. Our advanced engineering and stringent quality standards ensure that every product meets the rigorous demands of the railway sector.
+                                    Rine Group of Industries is a premier manufacturing company specializing in the production of highly critical casting and electromechanical products for the Indian Railways. With an unwavering commitment to precision and quality, we deliver a range of essential components, including couplers, knuckles, axle boxes, alternators, oil cooling units, and control panels. Our advanced engineering and stringent quality standards ensure that every product meets the rigorous demands of the railway sector.
                                 </p>
                                 <p>
-                                With over 250 skilled professionals driving innovation and excellence, Rine Group continues to be a trusted partner to the Indian Railways, contributing to the nation’s transportation infrastructure with reliable, high-performance solutions.
+                                    With over 250 skilled professionals driving innovation and excellence, Rine Group continues to be a trusted partner to the Indian Railways, contributing to the nation’s transportation infrastructure with reliable, high-performance solutions.
                                 </p>
                                 {/* <p>
                                     With a passion for innovation and a dedication to excellence, we strive
@@ -95,33 +95,39 @@ const AboutUs = () => {
                 </div>
 
 
-                <div className="w-full h-fit lg:h-fit lg:py-10 border border-[#0A173B] bg-black bg-cover bg-fixed mt-[10%] flex  relative font-raleway" 
-                style={{
-                    backgroundImage: "url('data:image/svg+xml,%3Csvg xmlns=%27http://www.w3.org/2000/svg%27 width=%27100%25%27 height=%27100%25%27 viewBox=%270 0 1600 800%27%3E%3Cg %3E%3Cpolygon fill=%27%2302050c%27 points=%271600 160 0 460 0 350 1600 50%27/%3E%3Cpolygon fill=%27%23040918%27 points=%271600 260 0 560 0 450 1600 150%27/%3E%3Cpolygon fill=%27%23060e23%27 points=%271600 360 0 660 0 550 1600 250%27/%3E%3Cpolygon fill=%27%2308122f%27 points=%271600 460 0 760 0 650 1600 350%27/%3E%3Cpolygon fill=%27%230A173B%27 points=%271600 800 0 800 0 750 1600 450%27/%3E%3C/g%3E%3C/svg%3E')"
-                }}>
+                <div
+                    className="w-full h-fit lg:h-fit lg:py-10 border border-[#0A173B] mt-[10%] flex relative font-raleway"
+                    style={{
+                        backgroundColor: '#111827',
+                        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23002B82' stroke-width='0.5'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23002B82'%3E%3Ccircle cx='769' cy='229' r='1'/%3E%3Ccircle cx='539' cy='269' r='1'/%3E%3Ccircle cx='603' cy='493' r='1'/%3E%3Ccircle cx='731' cy='737' r='1'/%3E%3Ccircle cx='520' cy='660' r='1'/%3E%3Ccircle cx='309' cy='538' r='1'/%3E%3Ccircle cx='295' cy='764' r='1'/%3E%3Ccircle cx='40' cy='599' r='1'/%3E%3Ccircle cx='102' cy='382' r='1'/%3E%3Ccircle cx='127' cy='80' r='1'/%3E%3Ccircle cx='370' cy='105' r='1'/%3E%3Ccircle cx='578' cy='42' r='1'/%3E%3Ccircle cx='237' cy='261' r='1'/%3E%3Ccircle cx='390' cy='382' r='1'/%3E%3C/g%3E%3C/svg%3E")`,
+                        backgroundSize: 'contain',
+                        opacity: 50
+                    }}
+                >
+
                     <div className=" border-white flex flex-col lg:flex-row w-[80%] mx-auto justify-between items-center gap-10 my-[5%] lg:my-[0%] lg:gap-10">
-                        <div className=" border-white w-[90%] lg:w-[40%] text-white font-railway flex gap-5 flex-col h-fit text-justify" data-aos="fade-right">
-                            <div className=" text-[27px] font-[500]">
+                        <div className=" border-white w-[90%] lg:w-[40%] text-white font-railway flex gap-5 flex-col h-fit text-left" data-aos="fade-right">
+                            <div className=" text-[30px] font-[500]">
                                 Message From Our Leader
                             </div>
 
-                            <div className="flex flex-col gap-5 text-[14px]">
+                            <div className="flex flex-col gap-5 text-[16px]">
                                 <p>
-                                At Rine Group of Industries, we take immense pride in our role as a trusted supplier to the Indian Railways. Over the past 30 years, our journey has been defined by a commitment to excellence, innovation, and the relentless pursuit of quality in everything we do. We have built a strong foundation rooted in delivering critical casting and electromechanical products that uphold the highest industry standards, ensuring safety and performance.
+                                    At Rine Group of Industries, we take immense pride in our role as a trusted supplier to the Indian Railways. Over the past 30 years, our journey has been defined by a commitment to excellence, innovation, and the relentless pursuit of quality in everything we do. We have built a strong foundation rooted in delivering critical casting and electromechanical products that uphold the highest industry standards, ensuring safety and performance.
                                 </p>
                                 <p>
-                                Our success is driven by the dedication of our talented workforce, whose expertise and passion are the backbone of our operations. Together, we have created a culture of continuous improvement, where each challenge is met with a solution-focused approach.
+                                    Our success is driven by the dedication of our talented workforce, whose expertise and passion are the backbone of our operations. Together, we have created a culture of continuous improvement, where each challenge is met with a solution-focused approach.
 
                                 </p>
                                 <p>
-                                As we move forward, we strive to become global leaders in the development and supply of highly critical casting products.
+                                    As we move forward, we strive to become global leaders in the development and supply of highly critical casting products.
                                 </p>
                             </div>
 
                             <div className="flex flex-col ">
-                                <p className="text-[20px] font-[500]">Sincerely,</p>
-                                <p className="text-[20px] font-[500]">Jai Gopal Goyal</p>
-                                <p className="text-[20px] font-[400]">Owner, Rine Group of Industries</p>
+                                <p className="text-[16px] ">Sincerely,</p>
+                                <p className="text-[16px] ">Jai Gopal Goyal</p>
+                                <p className="text-[16px] ">Owner, Rine Group of Industries</p>
                             </div>
 
                         </div>
@@ -150,7 +156,7 @@ const AboutUs = () => {
                         </span>
 
                         <p className="font-raleway text-[15px] font-[400] text-justify">
-                        At Rine Group of Industries, our mission is to deliver excellence in supplying high-quality casting and electromechanical products, ensuring safety and reliability for the Indian Railways and other sectors we serve. We are dedicated to fostering a culture of innovation, continuous improvement, and quality, driven by the expertise and passion of our talented workforce. Our commitment lies in maintaining the highest industry standards while providing solutions that meet critical operational needs.
+                            At Rine Group of Industries, our mission is to deliver excellence in supplying high-quality casting and electromechanical products, ensuring safety and reliability for the Indian Railways and other sectors we serve. We are dedicated to fostering a culture of innovation, continuous improvement, and quality, driven by the expertise and passion of our talented workforce. Our commitment lies in maintaining the highest industry standards while providing solutions that meet critical operational needs.
                         </p>
                     </div>
                 </div>
@@ -169,7 +175,7 @@ const AboutUs = () => {
                         </span>
 
                         <p className="font-raleway text-[15px] font-[400] text-justify">
-                        Our vision is to become a global leader in the development and supply of highly critical casting products, known for our unwavering focus on quality, innovation, and customer satisfaction. We aim to expand our reach, building on our 30 years of experience, to set new benchmarks in the industry and contribute to the progress of sectors that rely on our products for safety and performance.
+                            Our vision is to become a global leader in the development and supply of highly critical casting products, known for our unwavering focus on quality, innovation, and customer satisfaction. We aim to expand our reach, building on our 30 years of experience, to set new benchmarks in the industry and contribute to the progress of sectors that rely on our products for safety and performance.
                         </p>
                     </div>
                 </div>
@@ -182,3 +188,20 @@ const AboutUs = () => {
     )
 }
 export default AboutUs
+
+
+// background-color: #0024F7;
+// background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%230221d4' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23041fb1' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23051c8d' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23071a6a' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23091747' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
+// background-attachment: fixed;
+// background-size: cover;
+
+
+
+
+// background-color: #0C182D;
+// background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2000' height='2000' viewBox='0 0 800 800'%3E%3Cg fill='none' %3E%3Cg stroke='%23002B82' stroke-width='17'%3E%3Cline x1='-8' y1='-8' x2='808' y2='808'/%3E%3Cline x1='-8' y1='792' x2='808' y2='1608'/%3E%3Cline x1='-8' y1='-808' x2='808' y2='8'/%3E%3C/g%3E%3Cg stroke='%23002a7d' stroke-width='16'%3E%3Cline x1='-8' y1='767' x2='808' y2='1583'/%3E%3Cline x1='-8' y1='17' x2='808' y2='833'/%3E%3Cline x1='-8' y1='-33' x2='808' y2='783'/%3E%3Cline x1='-8' y1='-783' x2='808' y2='33'/%3E%3C/g%3E%3Cg stroke='%23002878' stroke-width='15'%3E%3Cline x1='-8' y1='742' x2='808' y2='1558'/%3E%3Cline x1='-8' y1='42' x2='808' y2='858'/%3E%3Cline x1='-8' y1='-58' x2='808' y2='758'/%3E%3Cline x1='-8' y1='-758' x2='808' y2='58'/%3E%3C/g%3E%3Cg stroke='%23002773' stroke-width='14'%3E%3Cline x1='-8' y1='67' x2='808' y2='883'/%3E%3Cline x1='-8' y1='717' x2='808' y2='1533'/%3E%3Cline x1='-8' y1='-733' x2='808' y2='83'/%3E%3Cline x1='-8' y1='-83' x2='808' y2='733'/%3E%3C/g%3E%3Cg stroke='%2300256e' stroke-width='13'%3E%3Cline x1='-8' y1='92' x2='808' y2='908'/%3E%3Cline x1='-8' y1='692' x2='808' y2='1508'/%3E%3Cline x1='-8' y1='-108' x2='808' y2='708'/%3E%3Cline x1='-8' y1='-708' x2='808' y2='108'/%3E%3C/g%3E%3Cg stroke='%23002469' stroke-width='12'%3E%3Cline x1='-8' y1='667' x2='808' y2='1483'/%3E%3Cline x1='-8' y1='117' x2='808' y2='933'/%3E%3Cline x1='-8' y1='-133' x2='808' y2='683'/%3E%3Cline x1='-8' y1='-683' x2='808' y2='133'/%3E%3C/g%3E%3Cg stroke='%23002264' stroke-width='11'%3E%3Cline x1='-8' y1='642' x2='808' y2='1458'/%3E%3Cline x1='-8' y1='142' x2='808' y2='958'/%3E%3Cline x1='-8' y1='-158' x2='808' y2='658'/%3E%3Cline x1='-8' y1='-658' x2='808' y2='158'/%3E%3C/g%3E%3Cg stroke='%2300215f' stroke-width='10'%3E%3Cline x1='-8' y1='167' x2='808' y2='983'/%3E%3Cline x1='-8' y1='617' x2='808' y2='1433'/%3E%3Cline x1='-8' y1='-633' x2='808' y2='183'/%3E%3Cline x1='-8' y1='-183' x2='808' y2='633'/%3E%3C/g%3E%3Cg stroke='%23001f5a' stroke-width='9'%3E%3Cline x1='-8' y1='592' x2='808' y2='1408'/%3E%3Cline x1='-8' y1='192' x2='808' y2='1008'/%3E%3Cline x1='-8' y1='-608' x2='808' y2='208'/%3E%3Cline x1='-8' y1='-208' x2='808' y2='608'/%3E%3C/g%3E%3Cg stroke='%23001d55' stroke-width='8'%3E%3Cline x1='-8' y1='567' x2='808' y2='1383'/%3E%3Cline x1='-8' y1='217' x2='808' y2='1033'/%3E%3Cline x1='-8' y1='-233' x2='808' y2='583'/%3E%3Cline x1='-8' y1='-583' x2='808' y2='233'/%3E%3C/g%3E%3Cg stroke='%23001c50' stroke-width='7'%3E%3Cline x1='-8' y1='242' x2='808' y2='1058'/%3E%3Cline x1='-8' y1='542' x2='808' y2='1358'/%3E%3Cline x1='-8' y1='-558' x2='808' y2='258'/%3E%3Cline x1='-8' y1='-258' x2='808' y2='558'/%3E%3C/g%3E%3Cg stroke='%23001a4b' stroke-width='6'%3E%3Cline x1='-8' y1='267' x2='808' y2='1083'/%3E%3Cline x1='-8' y1='517' x2='808' y2='1333'/%3E%3Cline x1='-8' y1='-533' x2='808' y2='283'/%3E%3Cline x1='-8' y1='-283' x2='808' y2='533'/%3E%3C/g%3E%3Cg stroke='%23001946' stroke-width='5'%3E%3Cline x1='-8' y1='292' x2='808' y2='1108'/%3E%3Cline x1='-8' y1='492' x2='808' y2='1308'/%3E%3Cline x1='-8' y1='-308' x2='808' y2='508'/%3E%3Cline x1='-8' y1='-508' x2='808' y2='308'/%3E%3C/g%3E%3Cg stroke='%23001741' stroke-width='4'%3E%3Cline x1='-8' y1='467' x2='808' y2='1283'/%3E%3Cline x1='-8' y1='317' x2='808' y2='1133'/%3E%3Cline x1='-8' y1='-333' x2='808' y2='483'/%3E%3Cline x1='-8' y1='-483' x2='808' y2='333'/%3E%3C/g%3E%3Cg stroke='%2300153c' stroke-width='3'%3E%3Cline x1='-8' y1='342' x2='808' y2='1158'/%3E%3Cline x1='-8' y1='442' x2='808' y2='1258'/%3E%3Cline x1='-8' y1='-458' x2='808' y2='358'/%3E%3Cline x1='-8' y1='-358' x2='808' y2='458'/%3E%3C/g%3E%3Cg stroke='%23001338' stroke-width='2'%3E%3Cline x1='-8' y1='367' x2='808' y2='1183'/%3E%3Cline x1='-8' y1='417' x2='808' y2='1233'/%3E%3Cline x1='-8' y1='-433' x2='808' y2='383'/%3E%3Cline x1='-8' y1='-383' x2='808' y2='433'/%3E%3C/g%3E%3Cg stroke='%23013' stroke-width='1'%3E%3Cline x1='-8' y1='392' x2='808' y2='1208'/%3E%3Cline x1='-8' y1='-408' x2='808' y2='408'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+// background-attachment: fixed;
+
+
+// background-color: #111827;
+// background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23002B82' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23111827'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E");
--- src/components/CertificationsList.jsx
+++ src/components/CertificationsList.jsx
@@ -303,10 +303,13 @@ const Certificate = ({ title, description, additionalInfo, imgPath }) => {
 
   return (
     <>
-      <div className="certificate-container flex flex-col-reverse justify-center items-center shadow-xl p-6 py-[5%] rounded-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out mb-0 gap-[50px] w-full bg-black bg-cover bg-fixed"
+      <div className="certificate-container flex flex-col-reverse justify-center items-center shadow-xl p-6 py-[5%] rounded-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out mb-0 gap-[50px] w-full"
         style={{
-          backgroundImage: "url('data:image/svg+xml,%3Csvg xmlns=%27http://www.w3.org/2000/svg%27 width=%27100%25%27 height=%27100%25%27 viewBox=%270 0 1600 800%27%3E%3Cg %3E%3Cpolygon fill=%27%2302050c%27 points=%271600 160 0 460 0 350 1600 50%27/%3E%3Cpolygon fill=%27%23040918%27 points=%271600 260 0 560 0 450 1600 150%27/%3E%3Cpolygon fill=%27%23060e23%27 points=%271600 360 0 660 0 550 1600 250%27/%3E%3Cpolygon fill=%27%2308122f%27 points=%271600 460 0 760 0 650 1600 350%27/%3E%3Cpolygon fill=%27%230A173B%27 points=%271600 800 0 800 0 750 1600 450%27/%3E%3C/g%3E%3C/svg%3E')"
-        }}
+          backgroundColor: '#111827',
+          backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='400' height='400' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23002B82' stroke-width='0.5'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23002B82'%3E%3Ccircle cx='769' cy='229' r='1'/%3E%3Ccircle cx='539' cy='269' r='1'/%3E%3Ccircle cx='603' cy='493' r='1'/%3E%3Ccircle cx='731' cy='737' r='1'/%3E%3Ccircle cx='520' cy='660' r='1'/%3E%3Ccircle cx='309' cy='538' r='1'/%3E%3Ccircle cx='295' cy='764' r='1'/%3E%3Ccircle cx='40' cy='599' r='1'/%3E%3Ccircle cx='102' cy='382' r='1'/%3E%3Ccircle cx='127' cy='80' r='1'/%3E%3Ccircle cx='370' cy='105' r='1'/%3E%3Ccircle cx='578' cy='42' r='1'/%3E%3Ccircle cx='237' cy='261' r='1'/%3E%3Ccircle cx='390' cy='382' r='1'/%3E%3C/g%3E%3C/svg%3E")`,
+          backgroundSize: 'contain',
+          opacity: 50
+      }}
       >
         <div className="w-fit flex flex-col gap-[50px] md:flex-row md:gap-[10%] h-fit mb-4 md:mb-0">
           {imgPath.map((item, index) => (
